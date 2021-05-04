a5ecc6e1eea8d437c5a01f809b2ccb457e19ccbc
justayar <cayar@ku.edu.tr>
2021-05-04 23:55:36 +0300

    MajorityProblem solved.

---
+++ src/_168_MajorityProblem.java
@@ -0,0 +1,45 @@
+public class _168_MajorityProblem {
+    /**
+     * Given an array nums of size n, return the majority element.
+     *
+     * The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+     *
+     *
+     * Example 1:
+     *
+     * Input: nums = [3,2,3]
+     * Output: 3
+     * Example 2:
+     *
+     * Input: nums = [2,2,1,1,1,2,2]
+     * Output: 2
+     *
+     *
+     * Constraints:
+     *
+     * n == nums.length
+     * 1 <= n <= 5 * 104
+     * -231 <= nums[i] <= 231 - 1
+     *
+     * Follow-up: Could you solve the problem in linear time and in O(1) space?
+     */
+
+    public int majorityElement(int[] nums) {
+        int majorityEl = nums[0];
+        int count = 1;
+        for(int i=1;i< nums.length;i++){
+            if(nums[i] == majorityEl){
+                count ++;
+                if(count > nums.length / 2)
+                    return majorityEl;
+            }else{
+                count --;
+                if(count == 0){
+                    majorityEl = nums[i];
+                    count++;
+                }
+            }
+        }
+        return majorityEl;
+    }
+}
